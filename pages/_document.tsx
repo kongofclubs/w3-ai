a0e2027df56834075718c1f45b43d14a8f0ce7ce
Juno <love.code.ninja@gmail.com>
2023-08-10 10:25:44 -0500

    updated meta

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,11 +2,20 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 import { Toaster } from 'react-hot-toast'
 
+import { fontMono, fontSans } from '@/lib/fonts'
+import { cn } from '@/lib/utils'
+
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="en" suppressHydrationWarning>
       <Head />
-      <body>
+      <body
+        className={cn(
+          'font-sans antialiased',
+          fontSans.variable,
+          fontMono.variable
+        )}
+      >
         <Toaster />
         <Main />
         <NextScript />
