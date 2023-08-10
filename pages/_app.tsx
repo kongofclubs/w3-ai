0944c169a11506ab73b60d5366b89e754cbc1f20
Juno <love.code.ninja@gmail.com>
2023-08-10 11:36:44 -0500

    updated font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,11 +5,20 @@ import { TailwindIndicator } from '@/components/tailwind-indicator'
 import { AppProviders } from '@/components/providers'
 import { Layout } from '@/components/layout'
 
+import { fontMono, fontSans } from '@/lib/fonts'
+import { cn } from '@/lib/utils'
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <AppProviders attribute="class" defaultTheme="system" enableSystem>
       <Layout>
-        <Component {...pageProps} />
+        <main className={cn(
+          'font-sans antialiased',
+          fontSans.variable,
+          fontMono.variable
+        )}>
+          <Component {...pageProps} />
+        </main>
       </Layout>
       <TailwindIndicator />
     </AppProviders>
